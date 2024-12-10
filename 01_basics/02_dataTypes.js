03ba95b973b8cd268d1a4e573dc3de477f8f06e9
Vinay Dahariya <87851946+vinaydahariya@users.noreply.github.com>
2024-12-10 09:04:45 +0000

    container config done

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,25 @@
+"use strict"; // treat all js code as newer version
+
+// alert(3+3) // we are using node js, not a browser
+
+console.log(3+3);
+
+console.log("vinay")
+
+let name = "vinay"
+let age = 18
+let isLoggedIn = false
+let state;
+
+// number => 2 to the power 53
+// bigint
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined =>
+// symbol => unique
+
+// object
+
+console.log(typeof "vinay") // output is string
+
