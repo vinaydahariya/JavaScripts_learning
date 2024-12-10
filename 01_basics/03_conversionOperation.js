986475d10f1227f7a54a1d3eab03f4c388ff25f2
Vinay Dahariya <87851946+vinaydahariya@users.noreply.github.com>
2024-12-10 09:20:57 +0000

    container config done

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,18 @@
+let score = "33abc"
+
+console.log(typeof score);
+console.log(typeof(score));
+
+let valueInNumber = Number(score);
+console.log(typeof valueInNumber);
+console.log(valueInNumber);
+
+
+/*
+    "33" => 33
+    "33abc" => NaN
+    true = 1
+
+*/
+
+
