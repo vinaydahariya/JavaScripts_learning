b827196c44f0de7cb36e64347ac217bdc0160fa7
Vinay Dahariya <87851946+vinaydahariya@users.noreply.github.com>
2024-12-11 09:43:36 +0000

    container config done

--- 01_basics/06_string.js
+++ 01_basics/06_string.js
@@ -5,3 +5,10 @@ const repoCount = 50
 
 console.log(`Hello my name is ${name} and my repo count is ${repoCount}`);
 
+const gameName = new String('vinayhc')
+
+console.log(gameName[0]);
+console.log(gameName.__proto__);
+
+console.log(gameName.length);
+console.log(gameName.toUpperCase());
