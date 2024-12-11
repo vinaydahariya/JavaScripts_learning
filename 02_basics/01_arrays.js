0636956627bf5b57bc1e0552768571808f88d76a
Vinay Dahariya <87851946+vinaydahariya@users.noreply.github.com>
2024-12-11 10:15:53 +0000

    container config done

--- 02_basics/01_arrays.js
+++ 02_basics/01_arrays.js
@@ -1,3 +1,10 @@
 // arrays
 
-console.log("arrays");
+const myArr = [0, 1, 2, 3, 4 ,5, true, "vinay"]
+console.log(myArr[6]);
+
+const myArr2 = new Array(1, 2, 3)
+
+// Array method
+myArr.push(6)
+console.log(myArr);
