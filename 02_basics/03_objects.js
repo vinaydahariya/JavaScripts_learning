0a6eb3d874fcbd3c50f1fe4c3bdb29a0b0ad9d92
Vinay Dahariya <87851946+vinaydahariya@users.noreply.github.com>
2024-12-12 11:04:56 +0000

    container config done

--- 02_basics/03_objects.js
+++ 02_basics/03_objects.js
@@ -3,8 +3,11 @@
 
 // object literals
 const JsUser = {
-    name: "Vinay"
+    name: "Vinay",
     age: 18,
     location: "bilaspur",
+    email: "vinay2023ca112@mnnit.ac.in"
 }
+
+console.log(JsUser.email);
 
